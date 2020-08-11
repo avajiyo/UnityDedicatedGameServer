b5ff8b9574be6e4815ca9e99f2cc86dadfc2202e
Ava <ava@avacado.page>
2020-08-11 14:29:09 +1000

    Initial set-up for a TCP connection between a dedicated server (cmd, local ip: 127.0.0.1) and a Unity client.

---
+++ GameServer/GameServer/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace GameServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Game Server";
+            Server.Start(10, 26950);
+            Console.ReadKey();
+        }
+    }
+}
